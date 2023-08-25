5a8bde51ffba821032ca933e251d2c63d87f7df2
Muhammet Arslan <muhammet.arsln@gmail.com>
2023-08-25 13:21:26 +0300

    better handler

--- internal/user/interfaces/ws/router.go
+++ internal/user/interfaces/ws/router.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"github.com/iammuho/natternet/internal/user"
+	"github.com/iammuho/natternet/pkg/errorhandler"
 
 	"github.com/dgrr/websocket"
 	"github.com/gofiber/fiber/v2"
@@ -11,9 +12,7 @@ import (
 
 type handler struct {
 	application *user.Application
-	conn        *websocket.Conn
 	clients     sync.Map
-	accessToken string
 }
 
 // NewWSHandler is the constructor for the ws handler
@@ -21,7 +20,6 @@ func NewWSHandler(application *user.Application) *handler {
 	return &handler{
 		application: application,
 		clients:     sync.Map{},
-		accessToken: "",
 	}
 }
 
@@ -33,12 +31,28 @@ func (h *handler) RegisterRoutes(f fiber.Router) {
 
 	// Create a path for the websocket authenticated connection /ws/:accessToken
 	f.Get("/ws/:accessToken", func(c *fiber.Ctx) error {
-		h.accessToken = c.Params("accessToken")
+		accessToken := c.Params("accessToken")
 
-		// convert fasthttp ctx to fiber ctx
+		claims, err := h.application.AppContext.GetJwtContext().ParseJWT(accessToken)
+		if err != nil {
+			return h.sendErrorResponse(c, errorhandler.InvalidAccessTokenErrorCode, err.Message.(string))
+		}
+
+		if claims == nil || claims["ID"] == nil {
+			return h.sendErrorResponse(c, errorhandler.InvalidAccessTokenErrorCode, errorhandler.InvalidAccessTokenMessage)
+		}
+
 		ctx := c.Context()
 		wServer.Upgrade(ctx)
 
+		ctx.SetUserValue("ID", claims["ID"])
+		h.clients.Store(ctx.ID(), c)
+
 		return nil
 	})
+}
+
+func (h *handler) sendErrorResponse(c *fiber.Ctx, code int, message string) error {
+	response := &errorhandler.Response{Code: code, Message: message, StatusCode: fiber.StatusUnauthorized}
+	return c.Status(fiber.StatusUnauthorized).JSON(response)
 }
--- internal/user/interfaces/ws/ws_handler.go
+++ internal/user/interfaces/ws/ws_handler.go
@@ -2,55 +2,14 @@ package ws
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/iammuho/natternet/internal/user/interfaces/ws/types"
-	"github.com/iammuho/natternet/pkg/errorhandler"
+	"go.uber.org/zap"
 
 	"github.com/dgrr/websocket"
-	"github.com/gofiber/fiber/v2"
 )
 
 func (h *handler) OnOpen(c *websocket.Conn) {
-	// Parse the JWT
-	claims, err := h.application.AppContext.GetJwtContext().ParseJWT(h.accessToken)
-
-	if err != nil {
-		eventModel := &types.WebsocketMessage{}
-		eventModel.New(types.MessageTypeError)
-		eventModel.Message = &errorhandler.Response{Code: errorhandler.InvalidAccessTokenErrorCode, Message: err, StatusCode: fiber.StatusUnauthorized}
-
-		// convert eventmodel to []byte
-		eventModelByte := eventModel.ToJson()
-
-		if _, err := c.Write(eventModelByte); err != nil {
-			log.Println(err)
-			return
-		}
-
-		return
-	}
-
-	if claims == nil || claims["ID"] == nil {
-
-		eventModel := &types.WebsocketMessage{}
-		eventModel.New(types.MessageTypeError)
-		eventModel.Message = &errorhandler.Response{Code: errorhandler.InvalidAccessTokenErrorCode, Message: errorhandler.InvalidAccessTokenMessage, StatusCode: fiber.StatusUnauthorized}
-
-		// convert eventmodel to []byte
-		eventModelByte := eventModel.ToJson()
-
-		if _, err := c.Write(eventModelByte); err != nil {
-			log.Println(err)
-			return
-		}
-
-		return
-	}
-
-	c.SetUserValue("ID", claims["ID"])
-	h.clients.Store(c.ID(), c)
-
 	// connection ID to string
 	connectionID := fmt.Sprintf("%d", c.ID())
 
@@ -65,7 +24,9 @@ func (h *handler) OnOpen(c *websocket.Conn) {
 	eventModelByte := eventModel.ToJson()
 
 	if _, err := c.Write(eventModelByte); err != nil {
-		log.Println(err)
+		h.application.AppContext.GetLogger().Error("Error sending connection successful event",
+			zap.String("connectionID", connectionID),
+			zap.Error(err))
 		return
 	}
 
@@ -73,9 +34,11 @@ func (h *handler) OnOpen(c *websocket.Conn) {
 
 func (h *handler) OnClose(c *websocket.Conn, err error) {
 	if err != nil {
-		log.Printf("%d closed with error: %s\n", c.ID(), err)
+		h.application.AppContext.GetLogger().Error("Error closing connection",
+			zap.Uint64("connectionID", c.ID()),
+			zap.Error(err))
 	} else {
-		log.Printf("%d closed the connection\n", c.ID())
+		h.application.AppContext.GetLogger().Info("Connection closed", zap.Uint64("connectionID", c.ID()))
 	}
 
 	h.clients.Delete(c.ID())
